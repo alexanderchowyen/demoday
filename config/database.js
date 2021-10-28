7cce52cd9d045a28a82ce920fc9f836ff9c38018
Alexander Chow-Yen <89674474+alexanderchowyen@users.noreply.github.com>
2021-10-28 00:21:50 -0400

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://demo:demo@cluster0.q2ojb.mongodb.net/treatyourself?retryWrites=true&w=majority', 
+    'dbName': 'treatyourself'
+};
